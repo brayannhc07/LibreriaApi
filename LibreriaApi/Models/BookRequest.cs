2c969109c613c892aace2beec47772db9f50827d
CarlosYATO <rrodriguez51@alumnos.uaq.mx>
2022-12-13 12:50:29 -0600

    Initial Commit and Genre Module added

---
+++ LibreriaApi/Models/BookRequest.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace LibreriaApi.Models {
+	public class GenreRequest {
+
+		[DisplayName( "Nombre" )]
+		[Required( ErrorMessage = "Es necesario indicar el {0} del género." )]
+		[MaxLength( 45, ErrorMessage = "El {0} del género excede el tamaño permitido({1})." )]
+		public string? Name { get; set; }
+		[DisplayName( "Url de la imagen" )]
+		[MaxLength( 250, ErrorMessage = "El {0} del género excede el tamaño permitido({1})." )]
+		public string? ImageUrl { get; set; }
+	}
+}
